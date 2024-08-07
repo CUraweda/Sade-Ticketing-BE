0bd0ee48949a4f841b95b1bda77646ccaf53e80f
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-08-07 17:33:31 +0700

    feat: access jwt expire

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,7 @@
 const constant = {
   MAX_LEN_PW: 10,
-  JWT_ACCESS_EXP: 15 * 60, // 15 menit
+  // JWT_ACCESS_EXP: 15 * 60, // 15 menit
+  JWT_ACCESS_EXP: 7 * 24 * 60 * 60, // 7 hari (temp)
   JWT_REFRESH_EXP: 30 * 24 * 60 * 60, // 30 hari
 };
 
