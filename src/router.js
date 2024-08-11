a1b9b1bef66509149eb430b2f861d3053a910e45
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-08-11 09:57:35 +0700

    feat: gen crud doctor session

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import authRouter from "./core/auth/auth.router.js";
 import doctorRouter from "./core/doctor/doctor.router.js";
 import specialismRouter from "./core/specialism/specialism.router.js";
 import locationRouter from "./core/location/location.router.js";
+import doctorsessionRouter from "./core/doctorsession/doctorsession.router.js";
 
 const r = Router();
 
@@ -12,6 +13,7 @@ r.use("/auth", authRouter);
 r.use("/role", roleRouter);
 r.use("/user", userRouter);
 r.use("/doctor", doctorRouter);
+r.use("/doctor-session", doctorsessionRouter);
 r.use("/specialism", specialismRouter);
 r.use("/location", locationRouter);
 
