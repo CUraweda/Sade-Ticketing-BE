3ad4497368043b89ef43fc202810acd2a22a40c4
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-08-12 16:27:34 +0700

    fix: add auth middleware

--- src/core/client/client.router.js
+++ src/core/client/client.router.js
@@ -3,6 +3,8 @@ import validatorMiddleware from "../../middlewares/validator.middleware.js";
 import clientController from "./client.controller.js";
 import clientValidator from "./client.validator.js";
 import { baseValidator } from "../../base/validator.base.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+
 const r = Router(),
   validator = clientValidator,
   controller = new clientController();
@@ -17,12 +19,14 @@ r.get("/show-one/:id", controller.findById);
 
 r.post(
   "/create",
+  authMiddleware(),
   validatorMiddleware({ body: validator.create }),
   controller.create
 );
 
 r.put(
   "/update/:id",
+  authMiddleware(),
   validatorMiddleware({ body: validator.update }),
   controller.update
 );
