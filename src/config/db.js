eaa07e1c8809bb615ae8435ca6ed9213eb9b5d41
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-11-28 10:41:27 +0700

    feat: trigger client on notif create

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { upBookingStatusOnUpdateInvoice } from "../middlewares/prisma/update-booking.js";
+import { sendNotif } from "../middlewares/prisma/send-notif.js";
 
 const prism = new PrismaClient().$extends({
   query: {
     invoice: {
       updateMany: upBookingStatusOnUpdateInvoice,
       update: upBookingStatusOnUpdateInvoice,
+    },
+    notification: {
+      create: sendNotif,
     },
   },
 });
