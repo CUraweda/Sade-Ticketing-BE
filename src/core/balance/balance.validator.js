c291c35b4f60a31b2f1a7428b3baa0f475cb34f2
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-11-30 22:06:56 +0700

    feat: crud balance

---
+++ src/core/balance/balance.validator.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const BalanceValidator = {
+  create: Joi.object({
+    // no-data
+  }),
+  update: Joi.object({
+    // no-data
+  }),
+};
+
+export default BalanceValidator;
