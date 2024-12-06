28bffea1f3b5acb4b26122b32ade9e65dbcd2d03
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-12-06 09:14:20 +0700

    feat: crud signature

---
+++ src/core/signature/signature.controller.js
@@ -0,0 +1,75 @@
+import BaseController from "../../base/controller.base.js";
+import { NotFound } from "../../lib/response/catch.js";
+import SignatureService from "./signature.service.js";
+import fs from "fs";
+
+class SignatureController extends BaseController {
+  #service;
+
+  constructor() {
+    super();
+    this.#service = new SignatureService();
+  }
+
+  findAll = this.wrapper(async (req, res) => {
+    const q = this.joinBrowseQuery(
+      req.query,
+      "where",
+      `user_id:${req.user.id}`
+    );
+    const data = await this.#service.findAll(q);
+    return this.ok(res, data, "Banyak Signature berhasil didapatkan");
+  });
+
+  findById = this.wrapper(async (req, res) => {
+    const data = await this.#service.findById(req.params.id, req.user.id);
+    if (!data) throw new NotFound("Signature tidak ditemukan");
+
+    return this.ok(res, data, "Signature berhasil didapatkan");
+  });
+
+  create = this.wrapper(async (req, res) => {
+    const payload = req.body;
+
+    payload["user_id"] = req.user.id;
+    payload["signature_img_path"] = req.file?.path ?? null;
+
+    const data = await this.#service.create(req.body);
+    return this.created(res, data, "Signature berhasil dibuat");
+  });
+
+  update = this.wrapper(async (req, res) => {
+    const prevData = await this.#service.findById(req.params.id, req.user.id);
+    if (!prevData) throw new NotFound("Signature tidak ditemukan");
+
+    if (req.file?.path) {
+      if (prevData.signature_img_path)
+        fs.unlink(up.path, (err) => {
+          if (err) {
+            console.error("ERR(file): ", err);
+          }
+        });
+      payload["signature_img_path"] = req.file?.path ?? null;
+    }
+
+    const data = await this.#service.update(req.params.id, req.body);
+    return this.ok(res, data, "Signature berhasil diperbarui");
+  });
+
+  delete = this.wrapper(async (req, res) => {
+    const prevData = await this.#service.findById(req.params.id, req.user.id);
+    if (!prevData) throw new NotFound("Signature tidak ditemukan");
+
+    if (prevData.signature_img_path)
+      fs.unlink(up.path, (err) => {
+        if (err) {
+          console.error("ERR(file): ", err);
+        }
+      });
+
+    const data = await this.#service.delete(req.params.id);
+    return this.noContent(res, "Signature berhasil dihapus");
+  });
+}
+
+export default SignatureController;
---
+++ src/core/signature/signature.router.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import validatorMiddleware from "../../middlewares/validator.middleware.js";
+import SignatureController from "./signature.controller.js";
+import SignatureValidator from "./signature.validator.js";
+import { baseValidator } from "../../base/validator.base.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+const r = Router(),
+  validator = SignatureValidator,
+  controller = new SignatureController();
+
+r.get(
+  "/show-all",
+  authMiddleware(),
+  validatorMiddleware({ query: baseValidator.browseQuery }),
+  controller.findAll
+);
+
+r.get("/show-one/:id", authMiddleware(), controller.findById);
+
+r.post(
+  "/create",
+  authMiddleware(),
+  uploader("/signatures", "image", 3000000).single("image"),
+  validatorMiddleware({ body: validator.create }),
+  controller.create
+);
+
+r.put(
+  "/update/:id",
+  authMiddleware(),
+  uploader("/signatures", "image", 3000000).single("image"),
+  validatorMiddleware({ body: validator.update }),
+  controller.update
+);
+
+r.delete("/delete/:id", authMiddleware(), controller.delete);
+
+const signatureRouter = r;
+export default signatureRouter;
