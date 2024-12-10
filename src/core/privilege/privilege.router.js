e98f1714b1e526d7bb7e98f356c1d092a786e470
CakraDarma <baguscakraa02@gmail.com>
2024-12-10 13:25:30 +0800

    feat: auth middleware in privilege

--- src/core/privilege/privilege.router.js
+++ src/core/privilege/privilege.router.js
@@ -3,31 +3,35 @@ import validatorMiddleware from "../../middlewares/validator.middleware.js";
 import privilegeController from "./privilege.controller.js";
 import privilegeValidator from "./privilege.validator.js";
 import { baseValidator } from "../../base/validator.base.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
 const r = Router(),
   validator = privilegeValidator,
   controller = new privilegeController();
 
 r.get(
   "/show-all",
+  authMiddleware(),
   validatorMiddleware({ query: baseValidator.browseQuery }),
   controller.findAll
 );
 
-r.get("/show-one/:id", controller.findById);
+r.get("/show-one/:id", authMiddleware(), controller.findById);
 
 r.post(
   "/create",
+  authMiddleware(["ADM", "SDM"]),
   validatorMiddleware({ body: validator.create }),
   controller.create
 );
 
 r.put(
   "/update/:id",
+  authMiddleware(["ADM", "SDM"]),
   validatorMiddleware({ body: validator.update }),
   controller.update
 );
 
-r.delete("/delete/:id", controller.delete);
+r.delete("/delete/:id", authMiddleware(["ADM", "SDM"]), controller.delete);
 
 const privilegeRouter = r;
 export default privilegeRouter;
--- src/core/serviceprice/serviceprice.router.js
+++ src/core/serviceprice/serviceprice.router.js
@@ -3,31 +3,35 @@ import validatorMiddleware from "../../middlewares/validator.middleware.js";
 import servicePriceController from "./serviceprice.controller.js";
 import servicePriceValidator from "./serviceprice.validator.js";
 import { baseValidator } from "../../base/validator.base.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
 const r = Router(),
   validator = servicePriceValidator,
   controller = new servicePriceController();
 
 r.get(
   "/show-all",
+  authMiddleware(),
   validatorMiddleware({ query: baseValidator.browseQuery }),
   controller.findAll
 );
 
-r.get("/show-one/:id", controller.findById);
+r.get("/show-one/:id", authMiddleware(), controller.findById);
 
 r.post(
   "/create",
+  authMiddleware(["ADM", "SDM"]),
   validatorMiddleware({ body: validator.create }),
   controller.create
 );
 
 r.put(
   "/update/:id",
+  authMiddleware(["ADM", "SDM"]),
   validatorMiddleware({ body: validator.update }),
   controller.update
 );
 
-r.delete("/delete/:id", controller.delete);
+r.delete("/delete/:id", authMiddleware(["ADM", "SDM"]), controller.delete);
 
 const servicepriceRouter = r;
 export default servicepriceRouter;
