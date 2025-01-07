ad193b237aac632183e67f55be3e5bf70c71f749
Diaz-Adrianz <diazadrianz191205@gmail.com>
2025-01-07 07:11:46 +0700

    refactor: reschedule

--- src/core/reschedulerequest/reschedulerequest.validator.js
+++ src/core/reschedulerequest/reschedulerequest.validator.js
@@ -1,20 +1,29 @@
 import Joi from "joi";
+import { relationExist } from "../../base/validator.base.js";
 
 export const RescheduleRequestValidator = {
   create: Joi.object({
     reason: Joi.string().required(),
-    schedule_id: Joi.string().uuid().required(),
-    start_date: Joi.date().iso(),
-    end_date: Joi.date().iso().min(Joi.ref("start_date")),
-    new_schedule_id: Joi.string().uuid(),
-    user_id: Joi.string().uuid().required(),
+    attendee_id: Joi.string()
+      .external(relationExist("scheduleAttendee"))
+      .required(),
+    new_schedule_id: Joi.string()
+      .external(relationExist("schedule"))
+      .required(),
   }),
   update: Joi.object({
-    reason: Joi.string(),
-    start_date: Joi.date().iso(),
-    end_date: Joi.date().iso().min(Joi.ref("start_date")),
-    new_schedule_id: Joi.string().uuid(),
-    is_approved: Joi.boolean(),
+    reason: Joi.string().required(),
+    attendee_id: Joi.string()
+      .external(relationExist("scheduleAttendee"))
+      .required(),
+    new_schedule_id: Joi.string()
+      .external(relationExist("schedule"))
+      .required(),
+  }),
+  adminResponse: Joi.object({
+    response: Joi.string().optional(),
+    is_approved: Joi.boolean().allow(null).optional(),
+    direct_to_new: Joi.boolean().optional(),
   }),
 };
 
