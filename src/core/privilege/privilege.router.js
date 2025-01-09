d478c126348c70628309d7f1dc85067f4fc3b676
Diaz-Adrianz <diazadrianz191205@gmail.com>
2025-01-09 18:48:55 +0700

    feat: privilege

--- src/core/privilege/privilege.router.js
+++ src/core/privilege/privilege.router.js
@@ -4,6 +4,7 @@ import privilegeController from "./privilege.controller.js";
 import privilegeValidator from "./privilege.validator.js";
 import { baseValidator } from "../../base/validator.base.js";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import { uploadPublic } from "../../middlewares/multer.middleware.js";
 const r = Router(),
   validator = privilegeValidator,
   controller = new privilegeController();
@@ -19,19 +20,21 @@ r.get("/show-one/:id", authMiddleware(), controller.findById);
 
 r.post(
   "/create",
-  authMiddleware(["ADM", "SDM"]),
+  authMiddleware(["SDM"]),
+  uploadPublic("/privilege-image", "image", 3000000).single("image_file"),
   validatorMiddleware({ body: validator.create }),
   controller.create
 );
 
 r.put(
   "/update/:id",
-  authMiddleware(["ADM", "SDM"]),
+  authMiddleware(["SDM"]),
+  uploadPublic("/privilege-image", "image", 3000000).single("image_file"),
   validatorMiddleware({ body: validator.update }),
   controller.update
 );
 
-r.delete("/delete/:id", authMiddleware(["ADM", "SDM"]), controller.delete);
+r.delete("/delete/:id", authMiddleware(["SDM"]), controller.delete);
 
 const privilegeRouter = r;
 export default privilegeRouter;
